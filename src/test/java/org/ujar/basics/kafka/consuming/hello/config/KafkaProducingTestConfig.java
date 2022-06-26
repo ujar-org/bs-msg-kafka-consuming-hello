c39670e47944d36b3d72bfc7e927e46bf1ef515a
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-06-26 18:46:32 +0300

    feat: configure generic ProducerFactory and KafkaTemplate

--- src/test/java/org/ujar/basics/kafka/consuming/hello/config/KafkaProducingTestConfig.java
+++ src/test/java/org/ujar/basics/kafka/consuming/hello/config/KafkaProducingTestConfig.java
@@ -1,10 +1,7 @@
 package org.ujar.basics.kafka.consuming.hello.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,36 +9,25 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerde;
 import org.ujar.basics.kafka.consuming.hello.consumer.dto.GreetingDto;
+import org.ujar.boot.starter.kafka.config.BaseKafkaProducingConfig;
 
 @Configuration
 @RequiredArgsConstructor
-public class KafkaTestConfig {
+public class KafkaProducingTestConfig extends BaseKafkaProducingConfig {
   @Bean
   ProducerFactory<String, GreetingDto> greetingMessageProducerFactory(KafkaProperties kafkaProperties) {
-    try (var serde = new JsonSerde<>(GreetingDto.class, new ObjectMapper())) {
-      var consumerProperties = kafkaProperties.getProducer().buildProperties();
-      var consumerFactory = new DefaultKafkaProducerFactory<>(consumerProperties,
-          new StringSerializer(),
-          serde.serializer());
-      consumerFactory.setTransactionIdPrefix(getTransactionPrefix());
-      return consumerFactory;
-    }
+    return producerFactory(GreetingDto.class, kafkaProperties);
   }
 
   @Bean
   KafkaTemplate<String, GreetingDto> greetingMessageKafkaTemplate(
       ProducerFactory<String, GreetingDto> greetingMessageProducerFactory) {
-    return new KafkaTemplate<>(greetingMessageProducerFactory);
+    return kafkaTemplate(greetingMessageProducerFactory);
   }
 
   @Bean
   KafkaOperations<Object, Object> errorHandlingKafkaTemplate() {
     return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(new HashMap<>()));
-  }
-
-  private String getTransactionPrefix() {
-    return "tx-" + UUID.randomUUID() + "-";
   }
 }
