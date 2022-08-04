976cc32f7ac409e11cbe9b76efc73a02f3c8d6af
Dima Denysenko <coding@dimdnk.com>
2022-08-04 11:46:03 +0300

    feat: improve config classes encapsulation

--- src/main/java/org/ujar/basics/kafka/consuming/hello/config/ApplicationConfig.java
+++ src/main/java/org/ujar/basics/kafka/consuming/hello/config/ApplicationConfig.java
@@ -3,5 +3,5 @@ package org.ujar.basics.kafka.consuming.hello.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConfig {
+class ApplicationConfig {
 }
